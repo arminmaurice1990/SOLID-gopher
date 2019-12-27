fb9c6fca9eef9d7df9250603d623d8548734ad98
Armin Maurice <armin@thinkdataworks.com>
2019-12-27 03:14:09 -0500

    merged server and route handler, since it has the single responsibilibty of changing when the routes change, rather than being spread over two packages

--- http-json-server/http_server/server.go
+++ http-json-server/http_server/server.go
@@ -1,24 +1,118 @@
 package http_server
 
 import (
-	"http-json-server/route_handler"
+	"encoding/json"
+	"http-json-server/logger"
+	"http-json-server/services/messages"
+	"http-json-server/services/todos"
+	"io"
+	"io/ioutil"
 	"net/http"
 	"strconv"
 )
 
 type http_server struct {
-	port   int
-	router route_handler.RouteHandler
+	port           int
+	todoservice    todos.TodoService
+	messageservice messages.MessageService
+	logger.Logger
 }
 
-func NewHttpServer(port int, router route_handler.RouteHandler) http_server {
-	return http_server{port: port, router: router}
+func NewHttpServer(port int, todoservice todos.TodoService, messageservice messages.MessageService, logger logger.Logger) http_server {
+	return http_server{
+		port:           port,
+		todoservice:    todoservice,
+		messageservice: messageservice,
+		Logger:         logger,
+	}
 }
 
 func (h http_server) Serve() error {
-	http.Handle("/todo", h.router.GetTodoRouteHandler())
-	http.Handle("/todos", h.router.ListTodosRouteHandler())
-	http.Handle("/messages", h.router.ListMessagesRouteHandler())
+	http.Handle("/todo", h.GetTodoRouteHandler())
+	http.Handle("/todos", h.ListTodosRouteHandler())
+	http.Handle("/messages", h.ListMessagesRouteHandler())
 
 	return http.ListenAndServe(strconv.Itoa(h.port), nil)
+}
+
+//request structs
+type GetTodoRequest struct {
+	id string
+}
+
+func (ro *http_server) GetTodoRouteHandler() http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ro.LogDebug("Get Todo Route called")
+		reqBody, err := GetRequestBodyBytes(r.Body)
+		if err != nil {
+			ro.LogError(err.Error(), r.Body)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		reqStruct := &GetTodoRequest{}
+		err = json.Unmarshal(reqBody, reqStruct)
+		if err != nil {
+			ro.LogError(err.Error(), r.Body)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		todo, err := ro.todoservice.GetTodo(r.Context(), "id")
+		if err != nil {
+			ro.LogError(err.Error(), r.Body)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		jsonBytes, err := json.Marshal(todo)
+		if err != nil {
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+		w.Write(jsonBytes)
+	})
+}
+
+func (ro *http_server) ListTodosRouteHandler() http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ro.LogDebug("Get Todos Route called")
+		todos, err := ro.todoservice.ListTodos(r.Context())
+		if err != nil {
+			ro.LogError(err.Error(), r.Body)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		jsonBytes, err := json.Marshal(todos)
+		if err != nil {
+			ro.LogError(err.Error(), todos)
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+		w.Write(jsonBytes)
+	})
+}
+
+func (ro *http_server) ListMessagesRouteHandler() http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ro.LogDebug("Get Messages Route called")
+		messages, err := ro.messageservice.ListMessages(r.Context())
+		if err != nil {
+			ro.LogError(err.Error(), r.Body)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		jsonBytes, err := json.Marshal(messages)
+		if err != nil {
+			ro.LogError(err.Error(), messages)
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+		w.Write(jsonBytes)
+	})
+}
+
+func GetRequestBodyBytes(reader io.Reader) ([]byte, error) {
+	reqBody, err := ioutil.ReadAll(reader)
+	if err != nil {
+		return []byte{}, nil
+	}
+	return reqBody, nil
 }
--- http-json-server/main.go
+++ http-json-server/main.go
@@ -3,23 +3,24 @@ package http_json_server
 import (
 	"http-json-server/datastore_connectors"
 	"http-json-server/http_server"
-	"http-json-server/route_handler"
+	"http-json-server/logger"
 	"http-json-server/services/messages"
 	"http-json-server/services/todos"
 	"log"
 )
 
 const port = 8080
+const debug = true
 
 func Main() {
 	db, err := datastore_connectors.ConnectPostgres()
 	if err != nil {
 		panic(err)
 	}
-	todoservice := todos.NewTodoService(db)
-	messagesservice := messages.NewMessageService()
-	routehandler := route_handler.NewRouteHandler(todoservice, messagesservice)
-	server := http_server.NewHttpServer(port, routehandler)
+	logHelper := logger.NewLogger(debug)
+	todoservice := todos.NewTodoService(db, logHelper)
+	messagesservice := messages.NewMessageService(logHelper)
+	server := http_server.NewHttpServer(port, todoservice, messagesservice, logHelper)
 
 	log.Fatal(server.Serve())
 }
